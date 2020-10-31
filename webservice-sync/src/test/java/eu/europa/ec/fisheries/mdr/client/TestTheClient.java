2bc67d7678bbc675c95b55035429d08930e128b4
Nikos Paraskevopoulos <Nikolaos.PARASKEVOPOULOS@sword-group.com>
2020-10-31 23:17:15 +0200

    [UNIONVMS-4810] Making the local WSDL work, added manual test

---
+++ webservice-sync/src/test/java/eu/europa/ec/fisheries/mdr/client/TestTheClient.java
@@ -0,0 +1,54 @@
+package eu.europa.ec.fisheries.mdr.client;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.namespace.QName;
+import java.net.URL;
+import java.util.List;
+
+import eu.europa.ec.mare.fisheries.services.MDREndPointService;
+import eu.europa.ec.mare.fisheries.services.mdr.v1.MDRDataNodeType;
+import eu.europa.ec.mare.fisheries.services.mdr.v1.MDRDataSetType;
+import eu.europa.ec.mare.fisheries.services.mdr.v1.MDRService;
+import org.junit.Test;
+
+public class TestTheClient {
+
+	// 1. Set this to the URL of the WSDL
+	private static final String WEBSERVICE_WSDL_LOCATION = "http://localhost:8080/mdr-service/v1/MDRService?wsdl";
+	// 2. Set this to the target namespace from the WSDL
+	private static final String WEBSERVICE_NAMESPACE = "http://services.fisheries.mare.ec.europa.eu/";
+	// 3. Set this to the "service" name from your WSDL
+	private static final String WEBSERVICE_NAME = "MDREndPointService";
+	// The properties above match the configuration in MDR.mdr_configuration
+
+	// 4. Activate any or both of the tests
+
+	/**
+	 * Test the generated classes by using them directly.
+	 */
+//	@Test
+	public void testManual() throws Exception {
+		MDREndPointService ss = new MDREndPointService(
+				new URL(WEBSERVICE_WSDL_LOCATION),
+				new QName(WEBSERVICE_NAMESPACE, WEBSERVICE_NAME)
+		);
+		final MDRService port = ss.getPort(new QName(WEBSERVICE_NAMESPACE, "MDREndPointPort"), MDRService.class);
+		final MDRDataSetType mdrList = port.getLatestVersionOfMDRList("FAO_AREA");
+		assertEquals("FAO_AREA", mdrList.getName());
+		assertTrue(mdrList.getContainedMDRDataNode().size() > 1);
+	}
+
+	/**
+	 * Test the generated classes by using them through the client.
+	 */
+//	@Test
+	public void testWithClient() throws Exception {
+		final MdrWebServiceClient mdrWebServiceClient = new MdrWebServiceClient();
+		mdrWebServiceClient.setWsdlLocation(new URL(WEBSERVICE_WSDL_LOCATION));
+		mdrWebServiceClient.setServiceName(new QName(WEBSERVICE_NAMESPACE, WEBSERVICE_NAME));
+		final List<MDRDataNodeType> result = mdrWebServiceClient.getMDRList("FAO_AREA");
+		assertTrue(result.size() > 1);
+	}
+}
