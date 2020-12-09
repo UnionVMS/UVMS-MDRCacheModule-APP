ab991dd1e532dd951d9b0dc56ecc17da66f32d41
nomikosi <42890529+nomikosi@users.noreply.github.com>
2020-12-09 19:23:40 +0200

    [UNIONVMS-MOVEMENT-RULES] added code for the three first rules

--- service/src/main/java/eu/europa/ec/fisheries/mdr/mapper/webservice/VPBrMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/mdr/mapper/webservice/VPBrMapper.java
@@ -1,15 +1,16 @@
 package eu.europa.ec.fisheries.mdr.mapper.webservice;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import eu.europa.ec.fisheries.mdr.entities.codelists.baseentities.MasterDataRegistry;
-import eu.europa.ec.fisheries.mdr.entities.codelists.sales.SaleBrDef;
+import eu.europa.ec.fisheries.mdr.entities.codelists.movement.VPBr;
 import eu.europa.ec.fisheries.mdr.qualifiers.MDRMapper;
 import eu.europa.ec.mare.fisheries.services.mdr.v1.MDRDataNodeType;
 
+import javax.enterprise.context.ApplicationScoped;
+
+
 @ApplicationScoped
-@MDRMapper("SALE_BR")
-public class SaleBrDefMapper extends MasterDataRegistryMapper {
+@MDRMapper("VP_BR")
+public class VPBrMapper extends MasterDataRegistryMapper {
 
     static final String CONTEXT = "CONTEXT";
     static final String FIELD = "FIELD";
@@ -18,16 +19,24 @@ public class SaleBrDefMapper extends MasterDataRegistryMapper {
     static final String BRSUBLEVEL = "BRSUBLEVEL";
     static final String FGVL_CODE = "FGVL_CODE";
     static final String FGVL_DESCRIPTION = "FGVL_DESCRIPTION";
+    static final String FGVT_CODE = "FGVT_CODE";
+    static final String FGVT_ENDESCRIPTION = "FGVT_ENDESCRIPTION";
+    static final String ACTIVEIND = "ACTIVEIND";
+
     @Override
     public MasterDataRegistry mapMDRDataNodeTypeToEntity(MDRDataNodeType mdrDataNodeType) {
-        SaleBrDef entity = new SaleBrDef();
+        VPBr entity = new VPBr();
         populateCommonProperties(mdrDataNodeType, entity);
+        entity.setContext(getProperty(mdrDataNodeType, CONTEXT));
         entity.setField(getProperty(mdrDataNodeType, FIELD));
         entity.setMessageIfFailing(getProperty(mdrDataNodeType, ENMESSAGE));
         entity.setSequenceOrder(getProperty(mdrDataNodeType, SEQORDER));
         entity.setBrSublevel(getProperty(mdrDataNodeType, BRSUBLEVEL));
         entity.setFluxGpValidationLevelCode(getProperty(mdrDataNodeType, FGVL_CODE));
         entity.setFluxGpValidationLevelEnDescr(getProperty(mdrDataNodeType, FGVL_DESCRIPTION));
+        entity.setFluxGpValidationTypeCode(getProperty(mdrDataNodeType, FGVT_CODE));
+        entity.setFluxGpValidationEnDescr(getProperty(mdrDataNodeType, FGVT_ENDESCRIPTION));
+        entity.setActivationIndicator(getProperty(mdrDataNodeType, ACTIVEIND));
         return entity;
     }
 }
