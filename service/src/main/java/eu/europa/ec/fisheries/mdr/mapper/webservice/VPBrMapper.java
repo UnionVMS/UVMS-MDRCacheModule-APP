dbcc50870965bfee9c8c0b369cb0601b52bc1d57
nomikosi <42890529+nomikosi@users.noreply.github.com>
2021-05-18 15:14:13 +0300

    [MOVEMENT-RULES-FIXES] fixed imports

--- service/src/main/java/eu/europa/ec/fisheries/mdr/mapper/webservice/VPBrMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/mdr/mapper/webservice/VPBrMapper.java
@@ -3,7 +3,7 @@ package eu.europa.ec.fisheries.mdr.mapper.webservice;
 import eu.europa.ec.fisheries.mdr.entities.codelists.baseentities.MasterDataRegistry;
 import eu.europa.ec.fisheries.mdr.entities.codelists.movement.VPBr;
 import eu.europa.ec.fisheries.mdr.qualifiers.MDRMapper;
-import eu.europa.ec.mare.fisheries.services.mdr.v1.MDRDataNodeType;
+import eu.europa.ec.mare.fisheries.model.mdr.v1.MDRDataNodeType;
 
 import javax.enterprise.context.ApplicationScoped;
 
